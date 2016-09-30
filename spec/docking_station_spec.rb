e3dd17b66a2a349dd3e95464e88df6ed33f8deb7
Ellie Hamilton <elliehamilton@notonthehighstreet.com>
2016-09-30 17:11:19 +0100

    Challenges complete up to 11 - added docking for bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,6 +7,14 @@ describe DockingStation do
 end
 
 describe DockingStation do
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.bike).to eq bike
+  end
+end
+
+describe DockingStation do
   it "releases working bikes" do
     bike = subject.release_bike
     expect(bike).to be_working
